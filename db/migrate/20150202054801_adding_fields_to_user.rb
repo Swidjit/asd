44450b69b245704124fefdccf27f4dad81c0d8d2
Swidjit <alex@swidjit.com>
2015-02-04 21:05:27 -0500

    fixing migration

--- db/migrate/20150202054801_adding_fields_to_user.rb
+++ db/migrate/20150202054801_adding_fields_to_user.rb
@@ -2,7 +2,6 @@ class AddingFieldsToUser < ActiveRecord::Migration
   def change
     add_column :users, :address, :string
     add_column :users, :credits, :integer
-    add_column :users, :about, :text
     add_column :users, :last_name, :string
   end
 end
