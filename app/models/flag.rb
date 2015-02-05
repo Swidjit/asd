f7194edb0e207632124456b130f4e91a9388e6c6
Swidjit <alex@swidjit.com>
2015-02-05 16:06:17 -0500

    user get notifications when they are flagged

--- app/models/flag.rb
+++ app/models/flag.rb
@@ -1,3 +1,14 @@
 class Flag < ActiveRecord::Base
   belongs_to :user
+
+  after_create :notify_user
+
+  def notify_user
+    notification = Notification.new
+    notification.notifier = self
+    notification.sender = self.user
+
+    notification.receiver = User.find(self.flagged_user)
+    notification.save!
+  end
 end
