f909e0e5134a67ee12f334ba2523895f2cc4fe92
Swidjit <alex@swidjit.com>
2015-06-03 18:54:12 -0400

    populating dealmaker choices from application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def dealmaker_options
+    return ["Cashbuyer", "Wholesaler", "Rehabber/Flipper", "JV Partner", "Buy and Hold Investor", "Contractor", "Title Agent", "Attorney", "Broker", "Hard Money Lender", "Owner/Developer", "Capital Provider", "Motivated Seller", "Intermediary", "Service Provider", "Contractor", "Architect", "Lender", "Investor", "Property Manager", "Asset Manager"]
+  end
 end
