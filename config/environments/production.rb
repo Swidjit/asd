ceb13b6ca93770a9222669c59ea97fb08e192a8e
Swidjit <alex@swidjit.com>
2015-06-19 07:54:09 -0400

    trying to force to https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
     }
   }
 
-  config.app_domain = 'quiet-refuge-7178.herokuapp.com'
+  config.app_domain = 'anysizedeals.herokuapp.com'
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -66,7 +66,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
