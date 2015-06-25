79364117025027f96a9cf515ad9400bc5216aecc
Swidjit <alex@swidjit.com>
2015-06-25 17:01:14 -0400

    updates to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,6 @@ Rails.application.configure do
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
-  config.action_mailer.default_url_options = {:host => 'www.anysizedeals.com'}
   # Code is not reloaded between requests.
   config.cache_classes = false
   config.paperclip_defaults = {
@@ -22,7 +21,7 @@ Rails.application.configure do
   }
 
   config.app_domain = 'anysizedeals.herokuapp.com'
-'
+  config.action_mailer.default_url_options = {:host => "www.anysizedeals.com"}
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
